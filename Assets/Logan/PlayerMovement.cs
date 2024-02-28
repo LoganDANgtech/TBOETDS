5b281a250f66fa06b2312aad1e75c0f30431530a
LoganDANgtech <147987555+LoganDANgtech@users.noreply.github.com>
2024-02-28 19:21:42 +0100

    yo

--- Assets/Logan/PlayerMovement.cs
+++ Assets/Logan/PlayerMovement.cs
@@ -15,30 +15,14 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
-    private Camera _mainCam;
-    private Vector3 _mousePos;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     private void Update()
     {
-        /*_camera = Camera.main;
-        Vector3 mouseWorldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPosition.z = 0f;
-        transform.LookAt(mouseWorldPosition);*/
-        
-        _mousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = _mousePos - transform.position;
-        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg - 90;
-        transform.rotation = Quaternion.Euler(0, 0, rotz);
-
-        /*_mousePos.x = 0f;
-        _mousePos.y = 0f;
-        transform.LookAt(_mousePos);*/
 
 
         _smoothedMovementInput = Vector2.SmoothDamp(
